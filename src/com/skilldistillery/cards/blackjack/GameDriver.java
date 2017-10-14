fa01aeea685e892bc944da2c5fc5a449135f02bb
David Stumpe <dstumpe264@gmail.com>
2017-10-14 13:31:25 -0600

    Added method to iterate over the player choosing to hit or stay

--- src/com/skilldistillery/cards/blackjack/GameDriver.java
+++ src/com/skilldistillery/cards/blackjack/GameDriver.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.cards.blackjack;
 
+import java.util.Scanner;
+
 import com.skilldistillery.cards.common.Card;
 import com.skilldistillery.cards.common.Deck;
 
@@ -8,21 +10,25 @@ public class GameDriver {
 	Player player;
 	Dealer dealer;
 	Card topCard;
-	/*User Story 2;
-	 * Add methods and fields to your classes that mimic the 
-	 * functionality of shuffling and dealing a deck of cards. 
-	 * Remember that when a card is dealt you have to remove 
-	 * it from the current deck! You should be able to print 
-	 * out a shuffled deck to the terminal.*/
+	Scanner sc;
+	/*
+	 * User Story 2; Add methods and fields to your classes that mimic the
+	 * functionality of shuffling and dealing a deck of cards. Remember that when a
+	 * card is dealt you have to remove it from the current deck! You should be able
+	 * to print out a shuffled deck to the terminal.
+	 */
 
 	public static void main(String[] args) {
+
 		GameDriver game = new GameDriver();
 		game.beginGame();
 		game.stageOne();
+		game.nextMove();
 	}
-	
+
 	void beginGame() {
-		//When the game starts it creates both players and a deck of cards.
+		sc = new Scanner(System.in);
+		// When the game starts it creates both players and a deck of cards.
 		deck = new Deck();
 		player = new Player();
 		dealer = new Dealer();
@@ -30,8 +36,10 @@ public class GameDriver {
 		deck.shuffleDeck();
 		System.out.println("The deck has been shuffled");
 	}
+
 	void stageOne() {
-		//this can be simplified into smaller methods // however at this time it's something i will come back to
+		// this can be simplified into smaller methods // however at this time it's
+		// something i will come back to
 		topCard = dealer.dealCard(deck);
 		player.hand.addCard(topCard);
 		System.out.println("Player's first card is: " + topCard);
@@ -46,10 +54,39 @@ public class GameDriver {
 		topCard = dealer.dealCard(deck);
 		dealer.hand.addCard(topCard);
 		System.out.println("Dealer has: " + topCard);
-		System.out.println("Dealer has: " + dealer.hand.getHand()); // here to make sure game logic is correct delete at release
+		System.out.println("Dealer has: " + dealer.hand.getHand()); // here to make sure game logic is correct delete at
+																	// release
 	}
+
+	// what comes next??
+	/*
+	 * Deal two hands of cards - one to a Dealer and another to a Player. Allow the
+	 * players to Hit (add cards to their hand) or Stay (not add cards to their
+	 * hand) in accordance to the rules of blackjack shown in the wiki. Do not worry
+	 * about the multiple values for Ace until your basic game logic is working.
+	 * Assume they are either 1 or 11 to start.
+	 */
 	void nextMove() {
-		
+		String input = " ";
+		char c = 'x';
+		// Allow player to hit or stay
+		System.out.println("Would you like to (H)it or (S)tay?");
+		input = sc.next();
+		do {
+			if ((input.equals("S")) || (input.equals("s")) || (input.equals("H")) || (input.equals("h"))) {
+				c = input.charAt(0);
+			} else {
+				System.out.println("You have not entered a proper input");
+			}
+			System.out.println("" + c + " " + input);
+
+			if (c == 'H' || c == 'h') {
+				dealer.dealCard(deck);
+				player.hand.addCard(topCard);
+			} else {
+				System.out.println("Dealers turn.");
+			}
+		} while (c == 'h' || c == 'H');
 	}
 
 }
